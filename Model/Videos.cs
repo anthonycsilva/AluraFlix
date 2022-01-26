64bcb985a4da0fefe478936540c03d1cb210c514
Anthony <anthony.eldyn@gmail.com>
2022-01-26 14:52:54 -0300

    removendo arquivos antigos

--- Model/Videos.cs
+++ Model/Videos.cs
@@ -3,7 +3,6 @@ using Model;
 using System.Data.SqlClient;
 using System.Data;
 using System;
-using Context;
 
 namespace Model{
 
@@ -41,41 +40,6 @@ namespace Model{
             Descricao = descricao;
             Url = url;
         }
-
-
-        public List<Videos> GetVideos(string QuerySQL){
-            List<Videos> listaVideos = new List<Videos>();
-            Banco bancoDeDados = new Banco();
-            SqlConnection connection = new SqlConnection(bancoDeDados.conexao_string);
-            try
-            {
-                SqlCommand comandoSQl = new SqlCommand(QuerySQL, connection);
-                connection.Open();
-                comandoSQl.CommandTimeout = 0;
-                var dados = comandoSQl.ExecuteReader();
-                var dt = new DataTable();
-                dt.Load(dados);
-
-                foreach (DataRow video in dt.Rows)
-                {
-                    string video_id = video["id"].ToString();
-                    string video_titulo = video["titulo"].ToString();
-                    string video_descricao = video["descricao"].ToString();
-                    string video_url = video["url"].ToString();
-                    var novoVideo = new Videos(video_titulo, video_descricao, video_url);
-                    listaVideos.Add(novoVideo);
-                }
-
-            }
-            catch (System.Exception)
-            {
-                
-                throw;
-            }
-
-            return listaVideos;
-        }
-
 
     }
 }
